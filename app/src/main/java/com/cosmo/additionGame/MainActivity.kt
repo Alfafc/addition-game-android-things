70eb7bc38bf7a5baf161b217e0f7e6afd64623ac
Fernando Costantini <alfafc@gmail.com>
2020-03-31 19:18:33 -0300

    first commit

---
+++ app/src/main/java/com/cosmo/additionGame/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.cosmo.additionGame
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "Hello World Started")
+
+        setContentView(R.layout.activity_main)
+
+        buttonRight.setOnClickListener {
+            changeText("Hola Tomás!")
+        }
+
+        buttonLeft.setOnClickListener {
+            changeText("Hola Mateo!")
+        }
+
+        timer = Timer()
+    }
+
+    fun changeText(newText: String) {
+        Toast.makeText(
+            this@MainActivity,
+            "Click $newText !",
+            Toast.LENGTH_SHORT
+        ).show()
+
+        textView.setText(newText)
+
+        val timerTask = object : TimerTask() {
+            override fun run() {
+                this@MainActivity.runOnUiThread({
+                    textView.setText(R.string.start_text)
+                })
+            }
+        }
+        timer.schedule(timerTask, 4000)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.cancel()
+        timer.purge()
+    }
+
+    companion object {
+        private lateinit var timer: Timer
+        private val TAG = MainActivity::class.java.simpleName
+    }
+}
