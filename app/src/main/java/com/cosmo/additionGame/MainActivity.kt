e76945eb1fb356dfcc1aeb0101049c92efe5ff13
Fernando Costantini <alfafc@gmail.com>
2020-03-31 22:27:52 -0300

    main activity kotlin update

--- app/src/main/java/com/cosmo/additionGame/MainActivity.kt
+++ app/src/main/java/com/cosmo/additionGame/MainActivity.kt
@@ -6,6 +6,8 @@ import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import kotlin.random.Random
+import kotlin.random.asJavaRandom
 
 class MainActivity : Activity() {
 
@@ -16,6 +18,10 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        buttonRight.visibility = android.view.View.INVISIBLE
+        buttonMiddle.visibility = android.view.View.INVISIBLE
+        buttonLeft.visibility = android.view.View.INVISIBLE
 
         timer.schedule(showNewOperationTimerTask(), 2000)
 
@@ -37,13 +43,31 @@ class MainActivity : Activity() {
 
         textView.setText("${currentOperation.firstValue} ${currentOperation.operator.sign} ${currentOperation.secondValue} = ?")
 
-        buttonLeft.setText(currentOperation.resultValue.toString())
-        buttonMiddle.setText(currentOperation.firstValue.toString())
-        buttonRight.setText(currentOperation.secondValue.toString())
+        val values = listOf(
+            currentOperation.resultValue,
+            currentOperation.resultValue + getRandomDelta(),
+            currentOperation.resultValue + getRandomDelta()
+        )
+        val shuffledValues = values.shuffled(Random.asJavaRandom())
+
+        buttonRight.visibility = android.view.View.VISIBLE
+        buttonMiddle.visibility = android.view.View.VISIBLE
+        buttonLeft.visibility = android.view.View.VISIBLE
+
+        buttonLeft.setText(shuffledValues[0].toString())
+        buttonMiddle.setText(shuffledValues[1].toString())
+        buttonRight.setText(shuffledValues[2].toString())
 
         buttonLeft.setEnabled(true)
         buttonMiddle.setEnabled(true)
         buttonRight.setEnabled(true)
+    }
+
+    private fun getRandomDelta(): Int {
+
+        if (Random.nextBoolean())
+            return Random.nextInt(1, 4);
+        return Random.nextInt(-4, -1);
     }
 
     fun checkResult(clickedButton: Button) {
@@ -52,6 +76,8 @@ class MainActivity : Activity() {
         val resultText: String
         if (currentOperation.resultValue.toString().equals(result)) {
             resultText = "Bien es $result!"
+
+            textView.setText("${currentOperation.firstValue} ${currentOperation.operator.sign} ${currentOperation.secondValue} = ${currentOperation.resultValue}")
 
             timer.schedule(showNewOperationTimerTask(), 2000)
         } else {
